27cd76355cd833612cb752a84dcfb3c69c41154d
Jacqui Maher <jacqui.maher@nytimes.com>
2014-07-07 15:48:44 -0400

    bugfix: make sure GOPATH/src exists on remote server

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 goproject CHANGELOG
 =====================
 
-0.1.0
+0.0.1
 -----
 - [jacqui] - Initial release of chef cookbook for very, very simplying deploying golang projects
+
+0.1.1
+-----
+- [jacqui] - bugfix: make sure the $GOPATH/src directory exists.
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,15 +23,21 @@ destination_directory = ""
 
 https_repo_match = node['goproject']['repository'].match(/^https:\/\/(.*?)\/(.*?)/)
 if https_repo_match && https_repo_match[1] && https_repo_match[2]
-  destination_directory = File.join(node['goproject']['gopath'], https_repo_match[1], https_repo_match[2])
+  destination_directory = File.join(node['goproject']['gopath'], "src", https_repo_match[1], https_repo_match[2])
 end
 
 git_repo_match = node['goproject']['repository'].match(/^git@(.*?):(.*?)/)
 if git_repo_match && git_repo_match[1] && git_repo_match[2]
-  destination_directory = File.join(node['goproject']['gopath'], git_repo_match[1], git_repo_match[2])
+  destination_directory = File.join(node['goproject']['gopath'], "src", git_repo_match[1], git_repo_match[2])
 end
 
 if !destination_directory.nil?
+  directory node['goproject']['gopath'] do
+    action :create
+    owner "ubuntu"
+    recursive true
+  end
+
   # grab the specified repository and branch of streamtools
   # then notify the next block to build and run it
   git destination_directory do
